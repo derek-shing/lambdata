c50eb3d20f0d4f244ddd3919596ac99bff7b0680
derek-shing <hpshing@gmail.com>
2019-03-21 10:19:25 -0700

    import class from other modules

--- lambdata_derek_shing/__init__.py
+++ lambdata_derek_shing/__init__.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-import SplitDate
+from .SplitDate import SplitDate
+from .SplitDate import SplitDateString
 
 ONES = pd.DataFrame(np.ones(10))
 ZEROS = pd.DataFrame(np.zeros(50))
